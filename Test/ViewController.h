3286d7fe21b08715346aa3800cd8ea8bba636aa7
tansaris <tan4778@gmail.com>
2014-09-29 12:57:54 +0700

    initial commit

---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Tan Ophaswongse on 9/29/14.
+//  Copyright (c) 2014 Tan Ophaswongse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by Tan Ophaswongse on 9/29/14.
+//  Copyright (c) 2014 Tan Ophaswongse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
